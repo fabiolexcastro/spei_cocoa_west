41808451ad464794f8f2cb1e0ea82c8cbd5f073b
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2022-01-20 07:25:30 -0500

    Add files via upload

--- 11 generate example of calc spei.R
+++ 11 generate example of calc spei.R
@@ -7,7 +7,7 @@
 # Load libraries ----------------------------------------------------------
 require(pacman)
 pacman::p_load(raster, rgdal, rgeos, glue, stringr, sf, tidyverse, fs, 
-               gtools, SPEI, future, furrr)
+               gtools, SPEI, future, furrr, ncdf4)
 
 g <- gc(reset = TRUE)
 rm(list = ls())
@@ -23,11 +23,25 @@ etps <- as.character(etps)
 prec <- grep('ppt', fles, value = TRUE)
 prec <- as.character(prec)
 prec <- prec[-grep('2020', prec)]
+shpf <- raster::shapefile('./data/shp/base/countries_target_4.shp')
+ghna <- shpf[shpf@data$NAME == 'Ghana',]
 
-# Calc etp  ---------------------------------------------------------------
+# Climate process ---------------------------------------------------------
 etps <- raster::stack(etps)
 prec <- raster::stack(prec)
+prec <- raster::crop(prec, ghna)
+prec <- raster::mask(prec, ghna)
+etps <- raster::crop(etps, ghna)
+etps <- raster::mask(etps, ghna)
+
+# A simple plot
+plot(prec[[1]])
+plot(etps[[1]])
+
+# Calc the balance --------------------------------------------------------
 baln <- prec - etps
+
+# Raster to table ---------------------------------------------------------
 tble <- rasterToPoints(baln, spatial = FALSE)
 tble <- as_tibble(tble)
 names(tble)
@@ -42,6 +56,12 @@ dtes <- flatten(dtes)
 dtes <- unlist(dtes)
 colnames(tble) <- c('x', 'y', dtes)
 
+# Write the results -------------------------------------------------------
+dir.create('./workspace/calc_spei_v1/raster/prec', recursive = TRUE)
+dir.create('./workspace/calc_spei_v1/raster/etps', recursive = TRUE)
+Map('writeRaster', x = unstack(prec), filename = glue('./workspace/calc_spei_v1/raster/prec/prec_{dtes}.tif'), overwrite = TRUE)
+Map('writeRaster', x = unstack(etps), filename = glue('./workspace/calc_spei_v1/raster/etps/etps_{dtes}.tif'), overwrite = TRUE)
+
 # Add gids and tidy the table
 tble <- mutate(tble, gid = 1:nrow(tble))
 tble <- gather(tble, date, value, -x, -y, -gid)
@@ -49,33 +69,8 @@ tble <- mutate(tble, year = str_sub(date, 2, 5), month = str_sub(date, 7, nchar(
 tble <- mutate(tble, year = as.numeric(year), month = as.numeric(month))
 gids <- unique(tble$gid)
 base <- tble %>% distinct(gid, x, y, date, year, month)
+saveRDS(object = tble, file = './workspace/calc_spei_v1/rds/tble_baln.rds')
 
-tble %>% drop_na()
 
-options(future.globals.maxSize= 8912896000)
-plan(cluster, workers = 40, gc = TRUE)
-spi <- furrr::future_map(.x = gids, .f = function(k){
-  cat('Start ', k, '\n')
-  bl <- filter(tble, gid == k)
-  bs <- filter(base, gid == k)
-  lt <- unique(bl$y)
-  bl <- pull(bl, value)
-  bl <- ts(bl)
-  
-  if(any(is.na(bl))){
-    
-    rs <- NA
-    
-  } else {
-    sp <- spei(bl, 1)
-    sp <- as.numeric(sp$fitted)
-    rs <- mutate(bs, spei = sp)
-  }
-  
-  cat('Done!\n')
-  return(rs)
-  
-})
-future:::ClusterRegistry(action = 'stop')
-  
+
 
